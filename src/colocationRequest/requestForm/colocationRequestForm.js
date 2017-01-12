7e937fc26c64c3489e75d614781e53af5858c019
JoaoPintoM <pinto.joao@outlook.com>
2017-01-12 13:57:54 +0100

    mit

--- src/colocationRequest/requestForm/colocationRequestForm.js
+++ src/colocationRequest/requestForm/colocationRequestForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { FormGroup, ControlLabel, FormControl, option } from 'react-bootstrap';
+import {
+  FormGroup, ControlLabel, FormControl, option, Radio
+} from 'react-bootstrap';
 
 class ColocationRequestForm extends Component {
   // constructor(props) {
@@ -20,29 +22,39 @@ class ColocationRequestForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             Code Postal:
-            <input type="text" defaultValue="1040"/>
-            <br/>
-            Prix Max:
-            <input type="number" defaultValue="" placeholder="500" />
-
-            <br/>
-
-            <FormGroup controlId="formControlsSelect">
-              <ControlLabel>Types de personnes:</ControlLabel>
-              <FormControl componentClass="select" placeholder="select">
-                 <option value="select">Geeky</option>
-                 <option value="other">Boring</option>
-                 <option value="other">Calmes</option>
-                 <option value="other">Bon vivants</option>
-              </FormControl>
-            </FormGroup>
-
-            <br/>
-            activité:
-            <input type="text" defaultValue="" />
           </label>
+          <input type="text" defaultValue="1040"/>
           <br/>
-          <input type="submit" value="Submit" />
+          Prix Max:
+          <input type="number" defaultValue="" placeholder="500" />
+
+          <br/>
+
+          <FormGroup controlId="formControlsSelect">
+            <ControlLabel>Types de personnes:</ControlLabel>
+            <FormControl componentClass="select" placeholder="select">
+               <option value="select">Geeky</option>
+               <option value="other">Boring</option>
+               <option value="other">Calmes</option>
+               <option value="other">Bon vivants</option>
+            </FormControl>
+          </FormGroup>
+
+          Préférence secteur habitants:
+          <input type="text" defaultValue="" placeholder="judiciaire  "/>
+          <br/>
+          <br/>
+
+          Hobbies:
+          <FormGroup>
+           <Radio inline>Vélo</Radio>
+           {' '}
+           <Radio inline>Sport</Radio>
+           {' '}
+           <Radio inline>Jeux vidéos</Radio>
+         </FormGroup>
+          <br/>
+          <input type="submit" value="Poster ma recherche" />
         </form>
       </div>
     )

5f2458d01a61d762c556954fd7a74267caab9993
Boubacar Barry <bouba@hackages.io>
2017-02-03 11:08:24 +0100

    currying createColocation

--- src/services/api.js
+++ src/services/api.js
@@ -1,31 +1,35 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const getAll = (dispatch) => {
   return () => {
     dispatch({
-      type: 'GET_ALL_START'})
+      type: 'GET_ALL_START'
+    })
 
     axios
-     .get('http://localhost:1338/api/users', { headers: {
-       'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-     }})
-     .then((response) => {
-       console.log(response.data[0].username)
+      .get('http://localhost:1338/api/users', {
+        headers: {
+          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+        }
+      })
+      .then((response) => {
+        console.log(response.data[0].username)
 
-       dispatch({
-         type: 'GET_ALL_SUCCESS',
-         payload: response.data
-       })
-     })
-     .catch((error) => {
-       console.log(error)
-       dispatch({
-         type: 'GET_ALL_ERROR'})
-     })
+        dispatch({
+          type: 'GET_ALL_SUCCESS',
+          payload: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+        dispatch({
+          type: 'GET_ALL_ERROR'
+        })
+      })
   }
 }
 
-export const createColocation = (dispatch, coloc) => {
+export const createColocation = (dispatch) => {
   console.log(coloc)
   console.log(coloc)
   console.log(coloc)
@@ -34,26 +38,33 @@ export const createColocation = (dispatch, coloc) => {
   console.log(coloc)
   console.log(coloc)
   console.log(coloc)
-  return () => {
+  /**
+   * currying the function, cad tu donnes les variables petit à petit
+   */
+  return (coloc) => {
     dispatch({
-      type: 'CREATE_COLOC'})
+      type: 'CREATE_COLOC'
+    })
 
     axios
-     .post('http://localhost:1338/api/colocations/create', {name: 'joao'}, { headers: {
-       'Authorization': 'Bearer ' + window.localStorage.getItem('token')
-     }})
-     .then((response) => {
-       console.log(response.data)
+      .post('http://localhost:1338/api/colocations/create', {name: 'joao'}, {
+        headers: {
+          'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+        }
+      })
+      .then((response) => {
+        console.log(response.data)
 
-       dispatch({
-         type: 'COLOCATION_CREATED',
-         payload: response.data
-       })
-     })
-     .catch((error) => {
-       console.log(error)
-       dispatch({
-         type: 'CREATE_COLOC_ERROR'})
-     })
+        dispatch({
+          type: 'COLOCATION_CREATED',
+          payload: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+        dispatch({
+          type: 'CREATE_COLOC_ERROR'
+        })
+      })
   }
 }

e41b461541c17e7055691ae4da16729ea9b3a84e
Joao Pinto <pinto.joao@outlook.com>
2017-02-24 11:07:25 +0100

    testing sliders

--- src/wall/wallContainer.js
+++ src/wall/wallContainer.js
@@ -5,6 +5,14 @@ import GeocodingSearchBox from '../shared/geocodingSearchBox'
 import { connect } from 'react-redux'
 import { getAll } from '../services/api'
 import { Col } from 'react-bootstrap'
+// import { Range } from 'rc-slider'
+
+const Slider = require('rc-slider');
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+
+import 'rc-slider/assets/index.css'
+// slider/index.css
 
 class WallContainer extends Component {
 
@@ -12,8 +20,16 @@ class WallContainer extends Component {
     this.props.getAll()
   }
 
+  handleSlider = (vals) => {
+    console.log(vals)
+  }
+
   handleNewAdress = (adress) => {
-    this.props.getAll(adress.location.lat, adress.location.lng)
+    if (!adress.location) {
+      this.props.getAll()
+    } else {
+      this.props.getAll(adress.location.lat, adress.location.lng)
+    }
   }
 
   render () {
@@ -25,8 +41,9 @@ class WallContainer extends Component {
           <h4>{r.name} - {r.price} €</h4>
           <img src={r.images[0]} role='presentation' style={imgStyle} />
           <br />
-          <span>Situé à: x</span>
-          <span>{' - '}</span>
+          <span>Situé à: </span>
+          <span>{' - '}{r.adress}</span>
+          <br />
           <span>Posté par: {r.user.username}</span>
           <br/>
           <img src={r.user.picture} role='presentation' style={userImgStyle} />
@@ -39,6 +56,7 @@ class WallContainer extends Component {
     return (
       <div>
         <h2>Wall</h2>
+          <Range min={0} max={20} defaultValue={[3, 10]} onAfterChange={this.handleSlider}/>
         <GeocodingSearchBox val="cumieira, portugal" onAdressSet={this.handleNewAdress} />
         <div>
           { this.props.colocs.length > 0 ? colocs : 'Loading' }

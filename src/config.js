5e800739f1f7b6262f676a3d9b3a501dcc0172f7
Joao Pinto <pinto.joao@outlook.com>
2017-03-17 10:30:50 +0100

    better config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
+const common = {
+  IMAGES_URL: 'http://res.cloudinary.com/stagounet/image/upload/',
+  IMG_MED_SCALE: 'c_scale,h_320,w_500',
+  JPG: '.jpg'
+}
+
 const dev = {
   API: 'http://stag.local:1338'
 }
@@ -6,7 +12,4 @@ const prod = {
   API: 'http://stag.ninja:1338'
 }
 
-console.log(dev)
-console.log(prod)
-
-export const config = dev
+export const config = Object.assign(common, dev)
